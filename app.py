6ba9756727e640aea5ce72b76cf6c29f85204858
Benjamin Gleitzman <gleitz@mit.edu>
2014-01-08 10:42:44 -0800

    Added support for reverse proxy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 
 from flaskutil import ReverseProxied
 
@@ -7,7 +7,10 @@ app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('base.html',
+                           favicon_url = url_for('static', filename='favicon.ico'),
+                           style_url = url_for('static', filename='style.css'),
+                           wallpaper_url = url_for('static', filename='wallpaper.jpg'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
