7a0c16a039911470025f0a4e26bf8360c1b2ee11
Benjamin Gleitzman <gleitz@mit.edu>
2014-01-08 19:00:51 -0800

    Update app for post-commit reloading

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template, url_for
+import json
+import os
+import signal
 
 from flaskutil import ReverseProxied
 
@@ -11,6 +14,23 @@ def home():
                            favicon_url = url_for('static', filename='favicon.ico'),
                            style_url = url_for('static', filename='style.css'),
                            wallpaper_url = url_for('static', filename='wallpaper.jpg'))
+
+@app.route('/fhqwhgads',methods=['POST'])
+def github_hook():
+    data = json.loads(request.data)
+    site_url = 'https://github.com/gleitz/mhs-reunion'
+    site_branch = 'master'
+    data_url = data.get('repository', {}).get('url')
+    data_branch = data.get('ref')
+    print data_url
+    print data_branch
+    if data_url == site_url and site_branch == data_branch:
+        print "Post-receive trigger. Exiting in 1 second"
+        os.system('git pull')
+        with open('/home/gleitz/projects/webapps/pid/mhs-reunion.pid', 'r') as f:
+            pid = int(f)
+            os.kill(pid, signal.SIGHUP)
+    return True
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
